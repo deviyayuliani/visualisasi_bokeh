d894d721222c07dda8f18e1f810dd23bde84ce3a
deviyayuliani <85496147+deviyayuliani@users.noreply.github.com>
2022-08-08 16:29:23 +0800

    Update COVIDinfo.py

--- COVIDinfo.py
+++ COVIDinfo.py
@@ -31,90 +31,8 @@ table_columns = [TableColumn(field='island', title='Pulau'),
 table = DataTable(source=src, columns=table_columns)
 tab1 = Panel(child=table, title='Summary Kasus Positif Tiap Pulau')
 
-# Tab 2: line & bar plot jumlah kasus per hari
 
-province_cds = ColumnDataSource(df_province)
-select_tools = ['wheel_zoom',
-                'box_select',
-                'lasso_select',
-                'poly_select',
-                'tap',
-                'reset']
-
-fig_line = figure(plot_height=600, plot_width=800,
-                  title='Jumlah Terkonfirmasi Positif Maret 2020',
-                  x_axis_label='Tanggal', y_axis_label='Jumlah kasus setiap pulau',
-                  x_range=df_province.get('date'),
-                  toolbar_location='right')
-
-fig_line.line(x='date', y='acc_confirmed',
-              line_color='black',
-              legend_label='Kasus akumulasi',
-              source=province_cds)
-
-fig_line.vbar(x='date', top='new_confirmed',
-              width=0.8,
-              alpha=0.3,
-              fill_color='#47A1BF',
-              legend_label='Terkonfirmasi positif harian',
-              source=province_cds)
-
-fig_line.vbar(x='date', top='new_released',
-              width=0.8,
-              alpha=0.3,
-              fill_color='#E332B4',
-              legend_label='Sembuh harian',
-              source=province_cds)
-
-fig_line.vbar(x='date', top='new_deceased',
-              width=0.8,
-              alpha=0.3,
-              fill_color='#99E332',
-              legend_label='Meninggal harian',
-              source=province_cds)
-
-
-fig_line.legend.location = 'top_left'
-
-fig_line.xaxis.major_label_orientation = math.pi/3
-
-tooltips = [('Provinsi', '@province_name'),
-           ('Pulau', '@island'),
-           ('Ibukota', '@capital_city'),
-           ('Total Kasus', '@acc_confirmed'),
-           ('Kasus baru', '@new_confirmed'),
-           ('Sembuh', '@new_released'),
-           ('Meninggal', '@new_deceased')]
-
-
-acc_hover_glyph = fig_line.circle(x='date', y='acc_confirmed', source=cases_cds,
-                                  size=15, alpha=0,
-                                  hover_fill_color='black', hover_alpha=0.5)
-
-new_confirmed_hover_glyph = fig_line.circle(x='date', y='new_confirmed', source=cases_cds,
-                                            size=15, alpha=0,
-                                            hover_fill_color='black', hover_alpha=0.1)
-
-new_released_hover_glyph = fig_line.circle(x='date', y='new_released', source=cases_cds,
-                                           size=15, alpha=0,
-                                           hover_fill_color='black', hover_alpha=0.1)
-
-new_deceased_hover_glyph = fig_line.circle(x='date', y='new_deceased', source=cases_cds,
-                                          size=15, alpha=0,
-                                          hover_fill_color='black', hover_alpha=0.1)
-
-
-fig_line.add_tools(HoverTool(tooltips=tooltips, renderers=[new_confirmed_hover_glyph,
-                                                           new_released_hover_glyph,
-                                                           new_deceased_hover_glyph,                                                           
-                                                           acc_hover_glyph]))
-
-fig_line.legend.click_policy = 'hide'
-
-tab2 = Panel(child=fig_line, title='Plot Jumlah Kasus')
-
-
-# Tab 3: scatter plot antara banyak kasus dan kepadatan penduduk
+# Tab 2: scatter plot antara banyak kasus dan kepadatan penduduk
 
 province_cds = ColumnDataSource(df_province)
 select_tools = ['wheel_zoom',
@@ -148,11 +66,26 @@ tooltips = [('Provinsi', '@province_name'),
 
 fig_scatter.add_tools(HoverTool(tooltips=tooltips))
 
-tab3 = Panel(child=fig_scatter, title='Populasi Per KM Persegi & Kasus Terkonfirmasi')
+tab2 = Panel(child=fig_scatter, title='Populasi Per KM Persegi & Kasus Terkonfirmasi')
+
+# Tab 3: line & bar plot jumlah kasus per hari
+
+#cases_cds = ColumnDataSource(df_cases)
+
+#fig_line = figure(plot_height=600, plot_width=800,
+                  #title='Jumlah Terkonfirmasi Positif Maret 2020',
+                  #x_axis_label='Tanggal', y_axis_label='Jumlah kasus',
+                  #x_range=df_cases.get('date'),
+                  #toolbar_location='right')
+
+#fig_line.line(x='date', y='acc_confirmed',
+              #line_color='black',
+              #legend_label='Kasus akumulasi',
+              #source=cases_cds)
 
 
 # Menggabungkan semua tab yang sudah dibuat
 
-tabs = Tabs(tabs=[tab1, tab2, tab3])
+tabs = Tabs(tabs=[tab1, tab2])
 
 curdoc().add_root(tabs)
