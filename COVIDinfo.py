6fc12058091468b31f77fd89f18780d08248e41e
deviyayuliani <85496147+deviyayuliani@users.noreply.github.com>
2022-08-08 16:20:15 +0800

    Update COVIDinfo.py

--- COVIDinfo.py
+++ COVIDinfo.py
@@ -33,64 +33,59 @@ tab1 = Panel(child=table, title='Summary Kasus Positif Tiap Pulau')
 
 # Tab 2: line & bar plot jumlah kasus per hari
 
-cases_cds = ColumnDataSource(df_cases)
+province_cds = ColumnDataSource(df_province)
+select_tools = ['wheel_zoom',
+                'box_select',
+                'lasso_select',
+                'poly_select',
+                'tap',
+                'reset']
 
 fig_line = figure(plot_height=600, plot_width=800,
                   title='Jumlah Terkonfirmasi Positif Maret 2020',
-                  x_axis_label='Tanggal', y_axis_label='Jumlah kasus',
-                  x_range=df_cases.get('date'),
+                  x_axis_label='Tanggal', y_axis_label='Jumlah kasus setiap pulau',
+                  x_range=df_province.get('date'),
                   toolbar_location='right')
 
 fig_line.line(x='date', y='acc_confirmed',
               line_color='black',
               legend_label='Kasus akumulasi',
-              source=cases_cds)
+              source=province_cds)
 
 fig_line.vbar(x='date', top='new_confirmed',
               width=0.8,
               alpha=0.3,
               fill_color='#47A1BF',
               legend_label='Terkonfirmasi positif harian',
-              source=cases_cds)
+              source=province_cds)
 
 fig_line.vbar(x='date', top='new_released',
               width=0.8,
               alpha=0.3,
               fill_color='#E332B4',
               legend_label='Sembuh harian',
-              source=cases_cds)
+              source=province_cds)
 
 fig_line.vbar(x='date', top='new_deceased',
               width=0.8,
               alpha=0.3,
               fill_color='#99E332',
               legend_label='Meninggal harian',
-              source=cases_cds)
+              source=province_cds)
 
-fig_line.vbar(x='date', top='new_tested',
-              width=0.8,
-              alpha=0.3,
-              fill_color='#FFD036',
-              legend_label='Tes harian',
-              source=cases_cds)
-
-fig_line.vbar(x='date', top='being_checked',
-              width=0.8,
-              alpha=0.3,
-              fill_color='#FC0339',
-              legend_label='Tes dalam proses',
-              source=cases_cds)
 
 fig_line.legend.location = 'top_left'
 
 fig_line.xaxis.major_label_orientation = math.pi/3
 
-tooltips = [('Total Kasus', '@acc_confirmed'),
-            ('Kasus baru', '@new_confirmed'),
-            ('Sembuh', '@new_released'),
-            ('Meninggal', '@new_deceased'),
-            ('Jumlah tes baru', '@new_tested'),
-            ('Dalam proses tes', '@being_checked')]
+tooltips = [('Provinsi', '@province_name'),
+           ('Pulau', '@island'),
+           ('Ibukota', '@capital_city'),
+           ('Total Kasus', '@acc_confirmed'),
+           ('Kasus baru', '@new_confirmed'),
+           ('Sembuh', '@new_released'),
+           ('Meninggal', '@new_deceased')]
+
 
 acc_hover_glyph = fig_line.circle(x='date', y='acc_confirmed', source=cases_cds,
                                   size=15, alpha=0,
@@ -108,19 +103,10 @@ new_deceased_hover_glyph = fig_line.circle(x='date', y='new_deceased', source=ca
                                           size=15, alpha=0,
                                           hover_fill_color='black', hover_alpha=0.1)
 
-new_tested_hover_glyph = fig_line.circle(x='date', y='new_tested', source=cases_cds,
-                                         size=15, alpha=0,
-                                         hover_fill_color='black', hover_alpha=0.1)
-
-being_checked_hover_glyph = fig_line.circle(x='date', y='being_checked', source=cases_cds,
-                                            size=15, alpha=0,
-                                            hover_fill_color='black', hover_alpha=0.1)
 
 fig_line.add_tools(HoverTool(tooltips=tooltips, renderers=[new_confirmed_hover_glyph,
                                                            new_released_hover_glyph,
-                                                           new_deceased_hover_glyph,
-                                                           new_tested_hover_glyph,
-                                                           being_checked_hover_glyph,
+                                                           new_deceased_hover_glyph,                                                           
                                                            acc_hover_glyph]))
 
 fig_line.legend.click_policy = 'hide'
